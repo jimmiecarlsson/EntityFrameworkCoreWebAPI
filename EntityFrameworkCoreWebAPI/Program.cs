d2896dba850d4919f611c2f196609c880f1c0e9d
Jimmie Carlsson <jimpa.c66@gmail.com>
2025-10-07 08:48:39 +0200

    Registrerade Services och Skapade migration av databas

--- EntityFrameworkCoreWebAPI/Program.cs
+++ EntityFrameworkCoreWebAPI/Program.cs
@@ -1,41 +1,33 @@
+using EntityFrameworkCoreWebAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Registrera Services
+builder.Services.AddOpenApi();
+builder.Services.AddDbContext<TodoDb>(options => options.UseSqlite("Data Source=todo.db"));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference(); // Scalar UI
 }
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
+app.MapGet("/todos", async (TodoDb db) => await db.TodoItems.ToListAsync());
+app.MapPost("/todos", async (TodoDb db, TodoItem item) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    db.TodoItems.Add(item);
+    await db.SaveChangesAsync();
+    return Results.Created($"/todos/{item.Id}", item);
+});
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
