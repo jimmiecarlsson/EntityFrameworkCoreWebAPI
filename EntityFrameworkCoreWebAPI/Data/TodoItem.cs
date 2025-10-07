0d25984bd71b49b4c1e5f8762d44cfe1dcdf2295
Jimmie Carlsson <jimpa.c66@gmail.com>
2025-10-07 08:27:27 +0200

    Skapa datamodell och DbContext

---
+++ EntityFrameworkCoreWebAPI/Data/TodoItem.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCoreWebAPI.Data
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public bool IsComplete { get; set; }
+
+    }
+
+    public class TodoDb : DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options) : base(options) { }
+        public DbSet<TodoItem> TodoItems => Set<TodoItem>();
+
+    }
+}
